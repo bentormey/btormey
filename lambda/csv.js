710f340b948ba040291099157ab0f53812f3091c
bentormey <43364789+bentormey@users.noreply.github.com>
2020-06-23 16:42:15 +0400

    Update csv.js

--- lambda/csv.js
+++ lambda/csv.js
@@ -4,7 +4,7 @@ const Papa = require('papaparse');
 const KEY = '2PACX-1vTYv2MZvIEjsa_jqJtxBrgSCdxhk16naCqSxBvExlTrNJS8ZbJi4e5N3F5_mdCnoGGBSSBz39c0DKhz';
 
 exports.handler = async (event, context) => {
-  const key request.QueryStringParameters["key"] || KEY;
+  const key = event.queryStringParameters.key || KEY;
   
   const csv = await fetch(`https://docs.google.com/spreadsheets/d/e/${key}/pub?output=csv`)
     .then(response => response.text());
