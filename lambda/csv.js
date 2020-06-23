fd9322679229c501534d3dd8efe83e0bfe2ea597
bentormey <43364789+bentormey@users.noreply.github.com>
2020-06-23 16:33:48 +0400

    Update csv.js

--- lambda/csv.js
+++ lambda/csv.js
@@ -1,14 +1,18 @@
 const fetch = require("node-fetch");
 const Papa = require('papaparse');
 
-const KEY = '2PACX-1vTYv2MZvIEjsa_jqJtxBrgSCdxhk16naCqSxBvExlTrNJS8ZbJi4e5N3F5_mdCnoGGBSSBz39c0DKhz';
+//const KEY = '2PACX-1vTYv2MZvIEjsa_jqJtxBrgSCdxhk16naCqSxBvExlTrNJS8ZbJi4e5N3F5_mdCnoGGBSSBz39c0DKhz';
 
 exports.handler = async (event, context) => {
-  return fetch(`https://docs.google.com/spreadsheets/d/e/${KEY}/pub?output=csv`)
+  const key request.QueryStringParameters["key"];
+  
+  const csv = await fetch(`https://docs.google.com/spreadsheets/d/e/${key}/pub?output=csv`)
     .then(response => response.text())
     .then(data => ({
       statusCode: 200,
       body: data
     }))
     .catch(error => ({ statusCode: 422, body: String(error) }));
+  
+  //const parsed = Papa.parse(csvData).data
 };
