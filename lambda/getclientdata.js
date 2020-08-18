6ae0b6f538128a1133ff3e33b107256b7e8f7881
bentormey <43364789+bentormey@users.noreply.github.com>
2020-08-18 10:57:04 +0400

    Update getclientdata.js

--- lambda/getclientdata.js
+++ lambda/getclientdata.js
@@ -10,6 +10,11 @@ exports.handler = async (event, context) => {
   
   return {
     statusCode: 200,
+    headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Methods': 'POST'
+          },
     body: csv
   };
 };
